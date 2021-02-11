fed63178f506cdeedec7eefa3081dca16fccc625
Pavel Chashkov <pavelchashkov@gmail.com>
2021-02-12 00:50:08 +0300

    Исправлена ошибка метода not_found_404_view; добавлен обработчик / в url; изменен формат ответа body

--- GbFramework/core/app.py
+++ GbFramework/core/app.py
@@ -1,8 +1,8 @@
 import logging
 
 
-def not_found_404_view():
-    return '404 WHAT', [b'404 PAGE Not Found']
+def not_found_404_view(request):
+    return '404 WHAT', '404 PAGE Not Found'
 
 
 class WebApp:
@@ -13,11 +13,12 @@ class WebApp:
     def __call__(self, environ, start_response):
         logging.debug(type(environ))
         logging.debug(environ)
-        path = environ['PATH_INFO']
+        tmp_path = environ['PATH_INFO']
+        path = tmp_path + '/' if not tmp_path.endswith('/') else tmp_path
         view = self.routes.get(path, not_found_404_view)
         request = {}
         for f_c in self.front_controllers:
             f_c(request)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
-        return body
+        return [body.encode('utf-8')]
