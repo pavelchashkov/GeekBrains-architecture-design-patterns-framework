977aba7ddf83ee83be519a7463f9c6770efad2a4
Pavel Chashkov <pavelchashkov@gmail.com>
2021-02-12 00:26:27 +0300

    Добавление паттерна front_controllers

--- GbFramework/core/app.py
+++ GbFramework/core/app.py
@@ -6,14 +6,18 @@ def not_found_404_view():
 
 
 class WebApp:
-    def __init__(self, routes):
+    def __init__(self, routes={}, front_controllers=[]):
         self.routes = routes
+        self.front_controllers = front_controllers
 
     def __call__(self, environ, start_response):
         logging.debug(type(environ))
         logging.debug(environ)
         path = environ['PATH_INFO']
         view = self.routes.get(path, not_found_404_view)
-        code, body = view()
+        request = {}
+        for f_c in self.front_controllers:
+            f_c(request)
+        code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return body
