7db91e1484651e8f2094b37ef435d21c36753da2
Pavel Chashkov <pavelchashkov@gmail.com>
2021-02-17 16:57:53 +0300

    Изменение проверки окончания строки url

--- GbFramework/core/app.py
+++ GbFramework/core/app.py
@@ -1,6 +1,3 @@
-import logging
-
-
 def not_found_404_view(request):
     return '404 WHAT', '404 PAGE Not Found'
 
@@ -11,10 +8,11 @@ class WebApp:
         self.front_controllers = front_controllers
 
     def __call__(self, environ, start_response):
-        logging.debug(type(environ))
-        logging.debug(environ)
-        tmp_path = environ['PATH_INFO']
-        path = tmp_path + '/' if not tmp_path.endswith('/') else tmp_path
+        for k, v in environ.items():
+            print(f'{k} - {v}')
+        path = environ['PATH_INFO']
+        if not path.endswith('/'):
+            path = f'{path}/'
         view = self.routes.get(path, not_found_404_view)
         request = {}
         for f_c in self.front_controllers:
